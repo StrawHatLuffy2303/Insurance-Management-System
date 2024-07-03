9080323cc7945688377974e1317878a8647d354d
Jayesh Verma <jayeshverma2303@gmail.com>
2024-07-04 01:13:10 +0530

    Add Initial file and Folder

---
+++ src/main/java/com/ims/insurance_management_system/InsuranceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ims.insurance_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ims/insurance_management_system/InsuranceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ims.insurance_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
