1e5323d633e92a6b8dea6f78f82132a4ba30d75d
Jayesh Verma <jayeshverma2303@gmail.com>
2024-07-04 16:01:36 +0530

    created interface method for register admin with exception

--- src/main/java/com/ims/insurance_management_system/Service/AdminService.java
+++ src/main/java/com/ims/insurance_management_system/Service/AdminService.java
@@ -1,5 +1,9 @@
 package com.ims.insurance_management_system.Service;
 
+import com.ims.insurance_management_system.Exception.UserException;
+import com.ims.insurance_management_system.Model.Admin;
+
 public interface AdminService {
-    
+    public Admin registerAdmin(Admin admin) throws UserException;
+
 }
