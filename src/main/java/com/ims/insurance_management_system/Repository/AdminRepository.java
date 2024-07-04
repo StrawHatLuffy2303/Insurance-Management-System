68063cc547e88415c8cd44ddc43b5cd473d0ffda
Jayesh Verma <jayeshverma2303@gmail.com>
2024-07-04 16:02:51 +0530

    created repository for admin

--- src/main/java/com/ims/insurance_management_system/Repository/AdminRepository.java
+++ src/main/java/com/ims/insurance_management_system/Repository/AdminRepository.java
@@ -1,9 +1,13 @@
 package com.ims.insurance_management_system.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ims.insurance_management_system.Model.Admin;
 
-public interface AdminRepository extends JpaRepository<Admin,Long>{
 
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin,Long>{
+        public Admin findByMobileNo(String mobileNo);
 }
