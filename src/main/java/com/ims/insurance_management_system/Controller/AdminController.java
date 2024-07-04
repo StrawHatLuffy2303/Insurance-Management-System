6d3baa6e7f2b4d5569b7e5836855d3e0154499ab
Jayesh Verma <jayeshverma2303@gmail.com>
2024-07-04 16:06:01 +0530

    created post method in controller file

--- src/main/java/com/ims/insurance_management_system/Controller/AdminController.java
+++ src/main/java/com/ims/insurance_management_system/Controller/AdminController.java
@@ -1,5 +1,28 @@
 package com.ims.insurance_management_system.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import com.ims.insurance_management_system.Exception.UserException;
+import com.ims.insurance_management_system.Model.Admin;
+import com.ims.insurance_management_system.Service.AdminService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("api/admin")
 public class AdminController {
-    
+
+    @Autowired
+    private AdminService adminservice;
+
+    @PostMapping("/")
+    public ResponseEntity<Admin> adminRegistration(@RequestBody Admin admin) throws UserException {
+        Admin newAdmin = adminservice.registerAdmin(admin);
+
+        return new ResponseEntity<Admin>(newAdmin, HttpStatus.CREATED);
+    }
+
 }
