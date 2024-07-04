e87f5cb0abf58071290186f9585906ce53c93fec
Jayesh Verma <jayeshverma2303@gmail.com>
2024-07-04 16:04:46 +0530

    created userexpection file

---
+++ src/main/java/com/ims/insurance_management_system/Exception/UserException.java
@@ -0,0 +1,12 @@
+package com.ims.insurance_management_system.Exception;
+
+public class UserException extends Exception {
+    public UserException() {
+
+    }
+        
+
+    public UserException(String message){
+        super(message);
+    }
+}
