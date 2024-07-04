d0986b5a65b807b2e3f1682cbaadfb376f529631
Jayesh Verma <jayeshverma2303@gmail.com>
2024-07-04 16:04:11 +0530

    created implementaion for service class for admin

---
+++ src/main/java/com/ims/insurance_management_system/Implementation/AdminServiceImpl.java
@@ -0,0 +1,27 @@
+package com.ims.insurance_management_system.Implementation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ims.insurance_management_system.Exception.UserException;
+import com.ims.insurance_management_system.Model.Admin;
+import com.ims.insurance_management_system.Repository.AdminRepository;
+import com.ims.insurance_management_system.Service.AdminService;
+
+@Service
+public class AdminServiceImpl implements AdminService {
+
+    @Autowired
+    private AdminRepository adminrepo;
+
+    @Override
+    public Admin registerAdmin(Admin admin) throws UserException {
+        Admin newAdmin=adminrepo.findByMobileNo(admin.getMobileNo());
+        if (newAdmin==null) {
+            return adminrepo.save(admin);
+        } else {
+            throw new UserException("You are already registered");
+        }
+    }
+
+}
