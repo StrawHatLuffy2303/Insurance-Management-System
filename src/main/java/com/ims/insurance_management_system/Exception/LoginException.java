ffb3841e6f54d43142bdee50c14961990cf9b327
Jayesh Verma <jayeshverma2303@gmail.com>
2024-07-05 12:54:25 +0530

    Created Login Exception

---
+++ src/main/java/com/ims/insurance_management_system/Exception/LoginException.java
@@ -0,0 +1,11 @@
+package com.ims.insurance_management_system.Exception;
+
+public class LoginException extends Exception {
+    public LoginException() {
+
+    }
+
+    public LoginException(String message) {
+        super(message);
+    }
+}
