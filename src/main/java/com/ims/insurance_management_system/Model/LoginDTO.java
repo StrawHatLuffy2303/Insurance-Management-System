6eea99462bc6bfd9e6f5d6b2bc54a228e5a7ea66
Jayesh Verma <jayeshverma2303@gmail.com>
2024-07-05 13:29:10 +0530

    login by mobile num not with name [Updated]

--- src/main/java/com/ims/insurance_management_system/Model/LoginDTO.java
+++ src/main/java/com/ims/insurance_management_system/Model/LoginDTO.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginDTO {
-     private String name;
+     private String mobileNo;
      private String password;
      
      @JsonIgnore
--- src/main/java/com/ims/insurance_management_system/Service/AdminService.java
+++ src/main/java/com/ims/insurance_management_system/Service/AdminService.java
@@ -3,6 +3,7 @@ package com.ims.insurance_management_system.Service;
 import com.ims.insurance_management_system.Exception.UserException;
 import com.ims.insurance_management_system.Model.Admin;
 
+
 public interface AdminService {
     public Admin registerAdmin(Admin admin) throws UserException;
 
