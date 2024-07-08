93ad71dd5a7529efe60dfc49b3b4db4e8ef9741c
Jayesh Verma <jayeshverma2303@gmail.com>
2024-07-08 19:50:30 +0530

    added service methods and implements the service method

--- src/main/java/com/ims/insurance_management_system/Implementation/ClientServiceImpl.java
+++ src/main/java/com/ims/insurance_management_system/Implementation/ClientServiceImpl.java
@@ -1,7 +1,55 @@
 package com.ims.insurance_management_system.Implementation;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.ims.insurance_management_system.Exception.LoginException;
+import com.ims.insurance_management_system.Exception.UserException;
+import com.ims.insurance_management_system.Model.Client;
+import com.ims.insurance_management_system.Model.CurrentUserSession;
+import com.ims.insurance_management_system.Model.LoginDTO;
+import com.ims.insurance_management_system.Repository.ClientRepository;
+import com.ims.insurance_management_system.Repository.SessionRepository;
 import com.ims.insurance_management_system.Service.ClientService;
 
-public class ClientServiceImpl implements ClientService{
-    
+public class ClientServiceImpl implements ClientService {
+
+    private ClientRepository clientRepo;
+
+    private SessionRepository sessionRepo;
+
+    @Override
+    public CurrentUserSession logIntoAccount(LoginDTO dto) throws UserException, LoginException {
+        Client existingClient = clientRepo.findByMobileNo(dto.getMobileNo());
+        if (existingClient == null) {
+            throw new LoginException("Mobile number not found in our records");
+        }
+
+        if (!dto.getPassword().equals(existingClient.getPassword())) {
+            throw new LoginException("Wrong Password");
+        }
+
+        if (sessionRepo.existsById(existingClient.getId())) {
+            throw new LoginException("You have already logged in");
+        }
+
+        String uniqueKey = UUID.randomUUID().toString().replace("-", "").substring(0, 7);
+        CurrentUserSession newCurrentUser = new CurrentUserSession(existingClient.getId(), "CLIENT", uniqueKey,
+                LocalDateTime.now());
+        sessionRepo.save(newCurrentUser);
+        return newCurrentUser;
+    }
+
+    @Override
+    public String logOutFromAccount(String key) throws UserException, LoginException {
+        CurrentUserSession activeSession = sessionRepo.findByUuid(key);
+        if (activeSession == null) {
+            throw new LoginException("No Active Session Found");
+        } else {
+            sessionRepo.delete(activeSession);
+            return "Logout SuccessFully";
+        }
+
+    }
+
 }
--- src/main/java/com/ims/insurance_management_system/Service/ClientService.java
+++ src/main/java/com/ims/insurance_management_system/Service/ClientService.java
@@ -2,7 +2,13 @@ package com.ims.insurance_management_system.Service;
 
 import org.springframework.stereotype.Service;
 
+import com.ims.insurance_management_system.Exception.LoginException;
+import com.ims.insurance_management_system.Exception.UserException;
+import com.ims.insurance_management_system.Model.CurrentUserSession;
+import com.ims.insurance_management_system.Model.LoginDTO;
+
 @Service
 public interface ClientService {
-
+   public CurrentUserSession logIntoAccount(LoginDTO dto) throws UserException,LoginException;
+   public String logOutFromAccount(String key) throws UserException,LoginException;
 }
