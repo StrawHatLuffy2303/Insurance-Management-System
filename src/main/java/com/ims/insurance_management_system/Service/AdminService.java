49cc9cdbd95dbd7c603d8541707c14cab8b55d7f
Jayesh Verma <jayeshverma2303@gmail.com>
2024-07-08 14:58:31 +0530

    created interface method for login account

--- src/main/java/com/ims/insurance_management_system/Service/AdminService.java
+++ src/main/java/com/ims/insurance_management_system/Service/AdminService.java
@@ -1,9 +1,12 @@
 package com.ims.insurance_management_system.Service;
 
+import com.ims.insurance_management_system.Exception.LoginException;
 import com.ims.insurance_management_system.Exception.UserException;
 import com.ims.insurance_management_system.Model.Admin;
+import com.ims.insurance_management_system.Model.CurrentUserSession;
+import com.ims.insurance_management_system.Model.LoginDTO;
 
 public interface AdminService {
     public Admin registerAdmin(Admin admin) throws UserException;
-
+    public CurrentUserSession logIntoAccount(LoginDTO dto) throws UserException,LoginException;
 }
