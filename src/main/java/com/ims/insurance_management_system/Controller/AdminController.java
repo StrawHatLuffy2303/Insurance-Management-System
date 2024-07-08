9e25012e3e660cbf114399ee91f9c1178e114051
Jayesh Verma <jayeshverma2303@gmail.com>
2024-07-08 14:55:58 +0530

    created login for admin

--- src/main/java/com/ims/insurance_management_system/Controller/AdminController.java
+++ src/main/java/com/ims/insurance_management_system/Controller/AdminController.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ims.insurance_management_system.Exception.LoginException;
 import com.ims.insurance_management_system.Exception.UserException;
 import com.ims.insurance_management_system.Model.Admin;
+import com.ims.insurance_management_system.Model.CurrentUserSession;
+import com.ims.insurance_management_system.Model.LoginDTO;
 import com.ims.insurance_management_system.Service.AdminService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +27,14 @@ public class AdminController {
         Admin newAdmin = adminservice.registerAdmin(admin);
 
         return new ResponseEntity<Admin>(newAdmin, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<CurrentUserSession> adminLogin(@RequestBody LoginDTO dto)
+            throws UserException,LoginException {
+        CurrentUserSession activeAdmin = adminservice.logIntoAccount(dto);
+
+        return new ResponseEntity<CurrentUserSession>(activeAdmin, HttpStatus.OK);
     }
 
 }
