5ffa6c1bdb5779626e4f1c7b679d6447a32f2d3d
Jayesh Verma <jayeshverma2303@gmail.com>
2024-07-11 19:56:58 +0530

    created insurance policy repository

--- src/main/java/com/ims/insurance_management_system/Repository/InsuranceRepository.java
+++ src/main/java/com/ims/insurance_management_system/Repository/InsuranceRepository.java
@@ -1,5 +1,11 @@
 package com.ims.insurance_management_system.Repository;
 
-public interface InsuranceRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.ims.insurance_management_system.Model.InsurancePolicy;
+
+@Repository
+public interface InsuranceRepository extends JpaRepository<InsurancePolicy, Long> {
+    InsurancePolicy findByPolicyNumber(String policyNumber);
 }
