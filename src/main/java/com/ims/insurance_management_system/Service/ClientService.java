9caddb108c18ade3a8c42197516b2e409c724979
Jayesh Verma <jayeshverma2303@gmail.com>
2024-07-12 11:50:54 +0530

    updates for Client service

--- src/main/java/com/ims/insurance_management_system/Service/ClientService.java
+++ src/main/java/com/ims/insurance_management_system/Service/ClientService.java
@@ -1,5 +1,8 @@
 package com.ims.insurance_management_system.Service;
 
+import java.util.List;
+
+import com.ims.insurance_management_system.Exception.ClientException;
 import com.ims.insurance_management_system.Exception.LoginException;
 import com.ims.insurance_management_system.Exception.UserException;
 import com.ims.insurance_management_system.Model.Client;
@@ -7,9 +10,18 @@ import com.ims.insurance_management_system.Model.CurrentUserSession;
 import com.ims.insurance_management_system.Model.LoginDTO;
 
 public interface ClientService {
-    public Client saveClient(Client client) throws UserException, LoginException;
+    public Client saveClient(Client client) throws ClientException;
+
+    List<Client> getAllClients(String key) throws LoginException,UserException;
+
+    public Client getClientById(Long id, String key) throws ClientException,LoginException;
+
+    public Client updateClient(Long id, Client client, String key) throws UserException ;
+
+    public Client deleteClient(Long id, String key) throws UserException,LoginException;
 
     public CurrentUserSession logIntoAccount(LoginDTO dto) throws UserException, LoginException;
 
     public String logOutFromAccount(String key) throws UserException, LoginException;
+
 }
